1f59c562ec057586906bb1b526265e9715336ce1
Hojoon Kim <hojoon0724@gmail.com>
2024-03-04 20:21:56 -0800

    jwt includes username

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const cors = require('cors');
 
 // Added dep
 const authCheck = require('./authCheck');
-const bookmarkController = require('./controllers/BookmarksCtrl');
-const userController = require('./controllers/UsersCtrl');
+const bookmarksController = require('./controllers/BookmarksCtrl');
+const usersController = require('./controllers/UsersCtrl');
 const PORT = process.env.PORT;
 
 // Sample data
@@ -22,8 +22,8 @@ app.use(cors());
 app.use(express.json());
 
 // Router
-app.use('/dashboard', authCheck, bookmarkController);
-app.use('/user', userController);
+app.use('/dashboard', authCheck, bookmarksController);
+app.use('/user', usersController);
 
 // Default Route
 app.get('/', (req, res) => {
